efcaf3f2c7f12f60dbf3eac2ba51d03d68842de2
kutluhanozcelik <kutluhanozcelik@hotmail.com>
2024-11-28 22:15:09 +0300

    Öklid mesafesi hesaplayıcı eklendi

---
+++ öklid.py
@@ -0,0 +1,21 @@
+import math
+
+points = [(4,4), (3,4), (1,0), (4,8)]
+
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+
+distances = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+print(f"Minimum mesafe: {min_distance}")
